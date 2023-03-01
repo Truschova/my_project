a122da62cfd6e01fa81b28334f681b808eb76268
Truschova <saphyana@inbox.ru>
2023-03-01 12:34:09 +0700

    xiaomi_himo

--- xiaomi_himo/js/main.js
+++ xiaomi_himo/js/main.js
@@ -1,7 +1,10 @@
 $(function(){
- $('.bike__slider').slick({
+ $('.bike__slider, .slider__item').slick({
   arrows: false,
   dots: true,
   fade: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
  });
+
 });
